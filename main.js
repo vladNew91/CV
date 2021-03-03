4b8ad48fbd95d3beac2f80588632a3ac7d5bff69
vladNew91 <vladnew91@gmail.com>
2021-03-04 00:26:42 +0300

    fix: adaptive 425px

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const arrayOfButtons = document.querySelectorAll('.showInfo');
 const arrayBloks = document.querySelectorAll('.hiddenBlock');
-const iconPlus = document.querySelectorAll('.material-icons');
+const iconPlus = document.querySelector('.plus');
+const changeIcon = (props) => iconPlus.textContent = props;
 
 for (let i = 0; i < arrayBloks.length; i++) {
   arrayOfButtons[i].addEventListener('click', () => {
     arrayBloks[i].classList.toggle('hiddenBlock');
+    changeIcon('add');
   })
 }
