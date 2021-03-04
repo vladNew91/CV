7662c65f7697b042285b5800c847ac34e3c80389
vladNew91 <vladnew91@gmail.com>
2021-03-05 02:08:12 +0300

    feat: add icons, light disco

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
 const arrayOfButtons = document.querySelectorAll('.showInfo');
 const arrayBloks = document.querySelectorAll('.hiddenBlock');
-const iconPlus = document.querySelector('.plus');
-const changeIcon = (props) => iconPlus.textContent = props;
 
 for (let i = 0; i < arrayBloks.length; i++) {
+  //change class
   arrayOfButtons[i].addEventListener('click', () => {
     arrayBloks[i].classList.toggle('hiddenBlock');
-    changeIcon('add');
+
+    //Change icon
+    const iconPlus = document.querySelector('.plus');
+    if (iconPlus.src == '../assets/remove-white-36dp.svg') {
+      return iconPlus.src = '../assets/add-white-36dp.svg';
+    } else {
+      return iconPlus.src = '../assets/remove-white-36dp.svg';
+    }
   })
 }
+
+
+//Change colors and shadows of name
+const arrayOfColors =  [
+  'rgb(255, 0, 0)', 
+  'rgb(255, 217, 0)', 
+  'rgb(179, 255, 0)', 
+  'rgb(228, 22, 149)', 
+  'rgb(209, 165, 238)',
+  'rgb(0, 255, 200)',
+  'rgb(255, 255, 255)' 
+];
+
+const arrayOfShadows = [
+  '1px 2px 3px rgb(255, 0, 0)', 
+  '1px 2px 3px rgb(255, 217, 0)', 
+  '1px 2px 3px rgb(179, 255, 0)', 
+  '1px 2px 3px rgb(228, 22, 149)',
+  '1px 2px 3px rgb(209, 165, 238)',
+  '1px 2px 3px rgb(0, 255, 200)',
+  '1px 2px 3px rgb(255, 255, 255)'
+];
+
+let i = 0;
+
+const changeStyle = document.querySelector('.changeColor');
+const lightDisko = () => {
+  //change color
+  changeStyle.style.color = arrayOfColors[i];
+  //change shadow
+  changeStyle.style.textShadow = arrayOfShadows[i];
+
+  i++;
+  if (i == arrayOfColors.length && i == arrayOfShadows.length) {
+    i = 0;
+  } 
+}
+
+setInterval(lightDisko, 4000);
