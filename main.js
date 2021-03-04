76a1659be83c65af47c42d26bb40a64a6a1080b5
vladNew91 <vladnew91@gmail.com>
2021-03-05 02:39:10 +0300

    fix: change all icons, hidden blocks

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ for (let i = 0; i < arrayBloks.length; i++) {
 
     //Change icon
     const iconPlus = document.querySelectorAll('.plus');
-    if (iconPlus[i].src == "https://silly-khorana-9b4b3f.netlify.app/assets/remove-white-36dp.svg") {
-      return iconPlus[i].src = "https://silly-khorana-9b4b3f.netlify.app/assets/add-white-36dp.svg";
-    } else {
+    if (iconPlus[i].src == "https://silly-khorana-9b4b3f.netlify.app/assets/add-white-36dp.svg") {
       return iconPlus[i].src = "https://silly-khorana-9b4b3f.netlify.app/assets/remove-white-36dp.svg";
+    } else {
+      return iconPlus[i].src = "https://silly-khorana-9b4b3f.netlify.app/assets/add-white-36dp.svg";
     }
   })
 }
