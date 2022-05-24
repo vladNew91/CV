5806b938515384af81e07d8a579bfe12f586f81a
Vladislav Nazarov <nazarov@aheadworks.com>
2022-05-24 18:11:38 +0300

    updates

--- main.js
+++ main.js
@@ -1,55 +1,53 @@
 //Change icons
-const arrayOfButtons = document.querySelectorAll('.showInfo');
-const arrayBloks = document.querySelectorAll('.hiddenBlock');
+const arrayOfButtons = [...document.getElementsByClassName('showInfo')];
+const arrayBloks = [...document.getElementsByClassName('hiddenBlock')];
 
-arrayOfButtons.forEach((el, i) => {
+arrayOfButtons.forEach((button, i) => {
   //change class
-  el.addEventListener('click', () => {
+  button.onclick = () => {
     arrayBloks[i].classList.toggle('hiddenBlock');
 
     const base = 'https://cv-uladzislau-nazarau.netlify.app/assets/'
 
     //Change icon
-    const iconPlus = document.querySelectorAll('.plus');       
-    if (iconPlus[i].src == base + 'add-white-36dp.svg') {
-      return iconPlus[i].src = base + 'remove-white-36dp.svg';
-    } else return iconPlus[i].src = base + 'add-white-36dp.svg';
-})});
+    const icons = document.getElementsByClassName('material-icons');       
+    if (icons[i].src == base + 'add-white-36dp.svg') {
+      return icons[i].src = base + 'remove-white-36dp.svg';
+    } else return icons[i].src = base + 'add-white-36dp.svg';
+}});
 
 //Change colors and shadows of name
-const arrayOfColors =  [
-  'rgb(255, 0, 0)', 
-  'rgb(255, 217, 0)', 
-  'rgb(179, 255, 0)', 
-  'rgb(228, 22, 149)', 
-  'rgb(209, 165, 238)',
-  'rgb(0, 255, 200)',
-  'rgb(255, 255, 255)' 
+const arrayOfColors = [
+  '#F7AEF8',
+  '#B388EB',
+  '#8093F1',
+  '#72DDF7',
+  '#F4F4ED',
 ];
 
 const arrayOfShadows = [
-  '1px 2px 3px rgb(255, 0, 0)', 
-  '1px 2px 3px rgb(255, 217, 0)', 
-  '1px 2px 3px rgb(179, 255, 0)', 
-  '1px 2px 3px rgb(228, 22, 149)',
-  '1px 2px 3px rgb(209, 165, 238)',
-  '1px 2px 3px rgb(0, 255, 200)',
-  '1px 2px 3px rgb(255, 255, 255)'
+  '1px 2px 3px #F7AEF8',
+  '1px 2px 3px #B388EB',
+  '1px 2px 3px #8093F1',
+  '1px 2px 3px #72DDF7',
+  '1px 2px 3px #F4F4ED',
 ];
 
 const changeStyle = document.getElementsByClassName('changeColor')[0];
 let i = 0;
 
-const lightDisko = () => {
+const changeColors = () => {
   //change color
   changeStyle.style.color = arrayOfColors[i];
   //change shadow
   changeStyle.style.textShadow = arrayOfShadows[i];
   i++;
   //repeat disco
-  if (i == arrayOfColors.length && i == arrayOfShadows.length) {
+  if (i == arrayOfColors.length || i == arrayOfShadows.length) {
     i = 0;
-  } 
-}
+  }
+};
+
+const lightDisko = () => changeColors();
 
 setInterval(lightDisko, 4000);
